e7fd1389287ffedb0c584083e4fca4a068e01b4b
涂强 <tuqiang.yuri@bytedance.com>
2021-06-21 17:22:14 +0800

    basic debug

---
+++ CMakeFiles/Tiny_JPEG_Decoder.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tiny_JPEG_Decoder.dir/src/jidctflt.c.o"
+  "CMakeFiles/Tiny_JPEG_Decoder.dir/src/loadjpeg.c.o"
+  "CMakeFiles/Tiny_JPEG_Decoder.dir/src/tinyjpeg.c.o"
+  "Tiny_JPEG_Decoder"
+  "Tiny_JPEG_Decoder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Tiny_JPEG_Decoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
